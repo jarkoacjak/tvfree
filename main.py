4c80502020b7ad4733d2d6673c608ebebe475ed7
jarkoacjak <videojarkohrarobloxacjak@gmail.com>
2026-03-22 12:24:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import urllib.parse
 import xbmcgui
 import xbmcplugin
 
-# --- Configuration (Kodi Engine) ---
+# --- Základné nastavenia ---
 HANDLE = int(sys.argv[1])
 BASE_URL = sys.argv[0]
 
 def add_directory_item(label, action, icon=None, is_folder=True, video_url=None):
-    """Vytvorí položku v menu Kodi. Názvy sú po slovensky."""
+    """Vytvorí položku v menu Kodi."""
     query = {'action': action}
     if video_url:
         query['url'] = video_url
@@ -27,13 +27,13 @@ def add_directory_item(label, action, icon=None, is_folder=True, video_url=None)
     xbmcplugin.addDirectoryItem(handle=HANDLE, url=url, listitem=list_item, isFolder=is_folder)
 
 def show_main_menu():
-    """Hlavné menu doplnku."""
+    """Hlavné menu."""
     add_directory_item("Slovenské TV", "list_sk", is_folder=True)
     add_directory_item("České TV", "list_cz", is_folder=False)
     xbmcplugin.endOfDirectory(HANDLE)
 
 def list_slovak_channels():
-    """Zoznam slovenských staníc s opraveným logom Senzi."""
+    """Zoznam slovenských staníc."""
     # TV JOJ
     joj_url = "https://live.cdn.joj.sk/live/andromeda/joj-1080.m3u8"
     joj_logo = "https://upload.wikimedia.org/wikipedia/commons/e/ee/Logo_TV_JOJ_-_2020.svg"
@@ -44,7 +44,7 @@ def list_slovak_channels():
     krimi_logo = "https://img.telkac.zoznam.sk/data/images/channel/2026/03/04/image_new_137.thumb.png"
     add_directory_item("JOJ Krimi", "play", icon=krimi_logo, is_folder=False, video_url=krimi_url)
 
-    # SENZI TV - Opravené logo podľa tvojho odkazu
+    # SENZI TV
     senzi_url = "https://lb.streaming.sk/senzi/stream/playlist.m3u8"
     senzi_logo = "https://static.wikia.nocookie.net/cstv/images/8/85/Senzi.png"
     add_directory_item("Senzi TV", "play", icon=senzi_logo, is_folder=False, video_url=senzi_url)
@@ -52,27 +52,27 @@ def list_slovak_channels():
     xbmcplugin.endOfDirectory(HANDLE)
 
 def play_video(stream_url, title):
-    """Spustí video s hlavičkami pre simuláciu prehliadača Chrome."""
+    """Spustí video s čistým nastavením hlavičiek."""
+    # User-Agent simulujúci Chrome
     ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
     
-    # Pridanie hlavičiek (nutné pre JOJ stanice)
-    headers = f"|User-Agent={urllib.parse.quote_plus(ua)}&Referer={urllib.parse.quote_plus('https://www.joj.sk/')}"
-    final_url = stream_url + headers
+    # Skúsime poslať hlavičky oddelene pre vyššiu stabilitu
+    final_url = stream_url + f"|User-Agent={ua}&Referer=https://www.joj.sk/"
     
     list_item = xbmcgui.ListItem(path=final_url)
     list_item.setInfo('video', {'title': title})
     
-    # Aktivácia InputStream Adaptive pre m3u8 streamy
+    # Nutné pre m3u8 streamy
     list_item.setProperty('inputstream', 'inputstream.adaptive')
     list_item.setProperty('inputstream.adaptive.manifest_type', 'hls')
     
     xbmcplugin.setResolvedUrl(HANDLE, True, list_item)
 
 def show_czech_notice():
-    """Zobrazí oznam pre České TV."""
+    """Zobrazí tvoj text pre České TV."""
     xbmcgui.Dialog().ok("TV Free", "Pripravujeme Čoskoro")
 
-# --- Router Logic ---
+# --- Smerovač (Router) ---
 if __name__ == '__main__':
     params = dict(urllib.parse.parse_qsl(sys.argv[2][1:]))
     action = params.get('action')
