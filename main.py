6384b65b875f767f2ef91c10426aebf9050bf2ba
jarkoacjak <videojarkohrarobloxacjak@gmail.com>
2026-04-12 13:44:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -48,6 +48,10 @@ def list_slovak_channels():
     add_directory_item("JOJ KRIMI", "play", icon="https://img.telkac.zoznam.sk/data/images/channel/2026/03/04/image_new_137.thumb.png", is_folder=False, video_url="https://live.cdn.joj.sk/live/andromeda/wau-1080.m3u8")
     add_directory_item("JOJ 24", "play", icon="https://img.joj.sk/38a52c95-84ce-4c04-b70a-2289a9fd1541", is_folder=False, video_url="https://live.cdn.joj.sk/live/andromeda/joj_news-1080.m3u8")
     add_directory_item("JOJ Šport", "play", icon="https://img.joj.sk/rx660n/662097da-11c1-434a-a923-3e00cdcb81e7", is_folder=False, video_url="https://live.cdn.joj.sk/live/andromeda/joj_sport-1080.m3u8")
+    
+    # NOVÁ STANICA: JOJ Šport 2
+    add_directory_item("JOJ Šport 2", "play", icon="https://static.hnonline.sk/images/slike/2025/12/04/o_4878486_1024.png", is_folder=False, video_url="https://live.cdn.joj.sk/live/andromeda/joj_sport2-1080.m3u8")
+    
     add_directory_item("Jojko", "play", icon="https://i.ibb.co/TxFWhc1J/jojko.png", is_folder=False, video_url="https://live.cdn.joj.sk/live/andromeda/jojko-1080.m3u8")
     add_directory_item("JOJ Family", "play", icon="https://i.ibb.co/hJgjKqpF/joj-family.png", is_folder=False, video_url="https://live.cdn.joj.sk/live/andromeda/family-1080.m3u8")
     add_directory_item("JOJ Cinema", "play", icon="http://www.mediaguru.cz/wp-content/uploads/2016/06/Joj-Cinema_akt.png", is_folder=False, video_url="https://live.cdn.joj.sk/live/andromeda/cinema-1080.m3u8")
@@ -61,8 +65,6 @@ def list_slovak_channels():
     add_directory_item("TV9", "play", icon="https://www.fotelka.tv/image/cache/catalog/Regionalne/TV9-240x234.jpg", is_folder=False, video_url="https://dash4.antik.sk/live/test_tv9/playlist.m3u8")
     add_directory_item("TV 8", "play", icon="https://www.digislovakia.sk/wp-content/uploads/2023/04/TV8-logo-2-300x231.png", is_folder=False, video_url="http://109.74.145.11:1935/tv8/ngrp:tv8.stream_all/playlist.m3u8")
     add_directory_item("Senzi TV", "play", icon="https://static.wikia.nocookie.net/cstv/images/8/85/Senzi.png", is_folder=False, video_url="https://lb.streaming.sk/senzi/stream/playlist.m3u8")
-    
-    # NOVÁ STANICA: Flow TV
     add_directory_item("Flow TV", "play", icon="https://www.flowtv.sk/wp-content/uploads/2021/04/logo_flow_tv_web.png", is_folder=False, video_url="https://app.viloud.tv/hls/channel/04e456809c83928443e59f0a2fce8610.m3u8")
     
     xbmcplugin.endOfDirectory(HANDLE)
