6936f20653fb5d23d0b2475b4e0bd7e5968ba612
jarkoacjak <videojarkohrarobloxacjak@gmail.com>
2026-05-05 15:27:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import sys
+import os
 import urllib.parse
+import xbmc
 import xbmcgui
 import xbmcplugin
+import xbmcvfs
 
-# --- Configuration (Kodi Engine) ---
+# --- Nastavenia ---
 HANDLE = int(sys.argv[1])
 BASE_URL = sys.argv[0]
+# Cesta, kde sa uloží vygenerovaný playlist
+ADDON_DATA_PATH = xbmcvfs.translatePath("special://profile/addon_data/plugin.video.jarko_tv/")
 
-def add_directory_item(label, action, icon=None, is_folder=True, video_url=None):
-    """Vytvorí položku v menu Kodi."""
+if not xbmcvfs.exists(ADDON_DATA_PATH):
+    xbmcvfs.mkdir(ADDON_DATA_PATH)
+
+def add_directory_item(label, action, icon=None, is_folder=True, video_url=None, tvg_id=""):
+    """Vytvorí položku v menu Kodi. tvg_id sa použije pre EPG."""
     query = {'action': action}
     if video_url:
         query['url'] = video_url
@@ -22,63 +30,86 @@ def add_directory_item(label, action, icon=None, is_folder=True, video_url=None)
     
     if not is_folder:
         list_item.setProperty('IsPlayable', 'true')
-        list_item.setInfo('video', {'title': label})
+        # Priradenie EPG ID pre zobrazenie v zozname
+        list_item.setInfo('video', {'title': label, 'plot': f"Stanica: {label}"})
 
     xbmcplugin.addDirectoryItem(handle=HANDLE, url=url, listitem=list_item, isFolder=is_folder)
+
+# --- DEFINÍCIA KANÁLOV PRE EPG A PLAYLIST ---
+# Formát: (Meno, Logo, EPG_ID, Stream_URL)
+CHANNELS_SK = [
+    ("TV JOJ", "https://yt3.googleusercontent.com/8rPXBoj2l1nhd9C-DCXF-s3tx0i_36GJzJcxeMyYvyPpPNakQsyc5DYc5d_QLDeI74ILkmFSJQ=s900-c-k-c0x00ffffff-no-rj", "JOJ.sk", "https://live.cdn.joj.sk/live/andromeda/joj-1080.m3u8"),
+    ("JOJ Plus", "https://i.ibb.co/21Xx2nnd/joj-plus.png", "JOJPlus.sk", "https://live.cdn.joj.sk/live/andromeda/plus-1080.m3u8"),
+    ("JOJ KRIMI", "https://img.telkac.zoznam.sk/data/images/channel/2026/03/04/image_new_137.thumb.png", "JOJKrimi.sk", "https://live.cdn.joj.sk/live/andromeda/wau-1080.m3u8"),
+    ("JOJ 24", "https://img.joj.sk/38a52c95-84ce-4c04-b70a-2289a9fd1541", "JOJ24.sk", "https://live.cdn.joj.sk/live/andromeda/joj_news-1080.m3u8"),
+    ("JOJ Šport", "https://img.joj.sk/rx660n/662097da-11c1-434a-a923-3e00cdcb81e7", "JOJSport.sk", "https://live.cdn.joj.sk/live/andromeda/joj_sport-1080.m3u8"),
+    ("Šport STVR", "https://lookaside.fbsbx.com/lookaside/crawler/media/?media_id=1451376403675507", "STV4.sk", "https://n16.stv.livebox.sk/stv-tv/_definst_/stv4-lq.smil/playlist.m3u8?auth=b64:X2FueV98MTc3Nzk5NDQwMHwyZjhmMWJiMmZjN2JjNGYwNmQ4NTE0YmJkNjAwM2I4ZTQyMjg0MWFh"),
+    ("Jojko", "https://i.ibb.co/TxFWhc1J/jojko.png", "Jojko.sk", "https://live.cdn.joj.sk/live/andromeda/jojko-1080.m3u8"),
+    ("JOJ Family", "https://i.ibb.co/hJgjKqpF/joj-family.png", "JOJFamily.sk", "https://live.cdn.joj.sk/live/andromeda/family-1080.m3u8"),
+    ("JOJ Cinema", "http://www.mediaguru.cz/wp-content/uploads/2016/06/Joj-Cinema_akt.png", "JOJCinema.sk", "https://live.cdn.joj.sk/live/andromeda/cinema-1080.m3u8"),
+    ("CS History", "https://img.joj.sk/418430b1-b598-40d1-8552-39b473c73836", "CSHistory.cz", "https://live.cdn.joj.sk/live/andromeda/cs_history-1080.m3u8"),
+    ("CS Film", "https://staticeu.sweet.tv/images/cache/channel_icons/BCTQOIAK/935-cs-film-hd.png", "CSFilm.cz", "https://live.cdn.joj.sk/live/andromeda/cs_film-1080.m3u8"),
+    ("CS Mystery", "https://www.jojgroup.sk/wp-content/uploads/CS-mistery.png", "CSMystery.cz", "https://live.cdn.joj.sk/live/andromeda/cs_mystery-1080.m3u8"),
+    ("Prima Love", "https://www.recenzer.cz/wp-content/uploads/2023/10/prima-love-logo.jpg", "PrimaLove.cz", "http://88.212.15.19/live/prima_love_avc_25p/playlist.m3u8"),
+    ("TV LUX", "https://213.sk/wp-content/uploads/2020/11/tvlux.jpg", "TVLux.sk", "https://stream.tvlux.sk/luxtv/luxtv-livestream/playlist.m3u8"),
+    ("TV Liptov", "https://yt3.googleusercontent.com/JJ6maA0dhvLU3z45Jhbgcc1brVZQswuPfYS6Da-Gli4MxXEPlhz5yuLkJlp7VL7mG7eSIxBORA=s900-c-k-c0x00ffffff-no-rj", "TVLiptov.sk", "http://95.105.255.137:1935/tvturiec/tvliptov.stream/playlist.m3u8"),
+    ("TV Nitrička", "https://www.satelitnatv.sk/wp-content/uploads/2013/04/nitricka.jpg", "TVNitricka.sk", "https://dash4.antik.sk/live/test_nitricka/playlist.m3u8"),
+    ("TV9", "https://www.fotelka.tv/image/cache/catalog/Regionalne/TV9-240x234.jpg", "TV9.sk", "https://dash4.antik.sk/live/test_tv9/playlist.m3u8"),
+    ("TV 8", "https://www.digislovakia.sk/wp-content/uploads/2023/04/TV8-logo-2-300x231.png", "TV8.sk", "http://109.74.145.11:1935/tv8/ngrp:tv8.stream_all/playlist.m3u8"),
+    ("Senzi TV", "https://static.wikia.nocookie.net/cstv/images/8/85/Senzi.png", "Senzi.sk", "https://lb.streaming.sk/senzi/stream/playlist.m3u8")
+]
+
+CHANNELS_CZ = [
+    ("Minimax", "https://www.minimaxcz.tv/storage/images/cWiGhWyxj8fFnyWQZxEX.png", "Minimax.cz", "http://88.212.15.19/live/test_minimax/playlist.m3u8"),
+    ("Óčko", "https://parasite.cz/wp-content/uploads/2013/02/ocko1.jpg", "Ocko.cz", "https://ocko-live-dash.ssl.cdn.cra.cz/cra_live2/ocko.stream.1.smil/playlist.m3u8"),
+    ("ČT 24", "https://pecka.tv/wp-content/uploads/2025/12/studio-ct24-400x600.jpg", "CT24.cz", "https://dash2.antik.sk/live/ct24_avc_25p/playlist.m3u8")
+]
+
+# --- FUNKCIE PRE PVR A EPG ---
+
+def setup_pvr_playlist():
+    """Vytvorí .m3u súbor s EPG tagmi."""
+    m3u_path = os.path.join(ADDON_DATA_PATH, "playlist.m3u")
+    try:
+        with open(m3u_path, "w", encoding="utf-8") as f:
+            f.write("#EXTM3U\n")
+            all_channels = CHANNELS_SK + CHANNELS_CZ
+            for name, logo, tid, url in all_channels:
+                f.write(f'#EXTINF:-1 tvg-id="{tid}" tvg-logo="{logo}",{name}\n{url}\n')
+        xbmcgui.Dialog().ok("PVR Playlist", f"Playlist bol úspešne vygenerovaný!\nCesta: {m3u_path}\n\nTeraz ho vložte do PVR IPTV Simple Client.")
+    except Exception as e:
+        xbmcgui.Dialog().error("Chyba", f"Nepodarilo sa vytvoriť playlist: {str(e)}")
+
+def setup_pvr_epg():
+    """Zobrazí adresu pre EPG XMLTV."""
+    epg_url = "https://itv.re/epg/sk_cz.xml.gz"
+    xbmcgui.Dialog().ok("EPG Nastavenie", f"Skopírujte tento odkaz do nastavení EPG v PVR klientovi:\n\n{epg_url}")
 
 # --- MENU STRUKTÚRA ---
 
 def show_main_menu():
-    """Hlavné menu doplnku."""
     add_directory_item("Živé vysielania", "live_menu", is_folder=True)
-    add_directory_item("Nastaviť playlist do PVR IPTV Simple Client priamo z pluginu", "set_pvr_playlist", is_folder=False)
+    add_directory_item("Nastaviť playlist do PVR IPTV Simple Client", "set_pvr_playlist", is_folder=False)
     add_directory_item("Nastaviť generované epg do PVR IPTV Simple Client", "set_pvr_epg", is_folder=False)
     xbmcplugin.endOfDirectory(HANDLE)
 
 def show_live_menu():
-    """Menu pod Živými vysielaniami."""
     add_directory_item("Slovenské TV", "list_sk", is_folder=True)
     add_directory_item("České TV", "list_cz", is_folder=True)
     xbmcplugin.endOfDirectory(HANDLE)
 
 def list_slovak_channels():
-    """Zoznam slovenských staníc."""
-    add_directory_item("TV JOJ", "play", icon="https://yt3.googleusercontent.com/8rPXBoj2l1nhd9C-DCXF-s3tx0i_36GJzJcxeMyYvyPpPNakQsyc5DYc5d_QLDeI74ILkmFSJQ=s900-c-k-c0x00ffffff-no-rj", is_folder=False, video_url="https://live.cdn.joj.sk/live/andromeda/joj-1080.m3u8")
-    add_directory_item("JOJ Plus", "play", icon="https://i.ibb.co/21Xx2nnd/joj-plus.png", is_folder=False, video_url="https://live.cdn.joj.sk/live/andromeda/plus-1080.m3u8")
-    add_directory_item("JOJ KRIMI", "play", icon="https://img.telkac.zoznam.sk/data/images/channel/2026/03/04/image_new_137.thumb.png", is_folder=False, video_url="https://live.cdn.joj.sk/live/andromeda/wau-1080.m3u8")
-    add_directory_item("JOJ 24", "play", icon="https://img.joj.sk/38a52c95-84ce-4c04-b70a-2289a9fd1541", is_folder=False, video_url="https://live.cdn.joj.sk/live/andromeda/joj_news-1080.m3u8")
-    add_directory_item("JOJ Šport", "play", icon="https://img.joj.sk/rx660n/662097da-11c1-434a-a923-3e00cdcb81e7", is_folder=False, video_url="https://live.cdn.joj.sk/live/andromeda/joj_sport-1080.m3u8")
-    add_directory_item("JOJ Šport 2", "play", icon="https://static.hnonline.sk/images/slike/2025/12/04/o_4878486_1024.png", is_folder=False, video_url="https://live.cdn.joj.sk/live/andromeda/joj_sport2-1080.m3u8")
-    add_directory_item("Jojko", "play", icon="https://i.ibb.co/TxFWhc1J/jojko.png", is_folder=False, video_url="https://live.cdn.joj.sk/live/andromeda/jojko-1080.m3u8")
-    add_directory_item("JOJ Family", "play", icon="https://i.ibb.co/hJgjKqpF/joj-family.png", is_folder=False, video_url="https://live.cdn.joj.sk/live/andromeda/family-1080.m3u8")
-    add_directory_item("JOJ Cinema", "play", icon="http://www.mediaguru.cz/wp-content/uploads/2016/06/Joj-Cinema_akt.png", is_folder=False, video_url="https://live.cdn.joj.sk/live/andromeda/cinema-1080.m3u8")
-    add_directory_item("CS History", "play", icon="https://img.joj.sk/418430b1-b598-40d1-8552-39b473c73836", is_folder=False, video_url="https://live.cdn.joj.sk/live/andromeda/cs_history-1080.m3u8")
-    add_directory_item("CS Film", "play", icon="https://staticeu.sweet.tv/images/cache/channel_icons/BCTQOIAK/935-cs-film-hd.png", is_folder=False, video_url="https://live.cdn.joj.sk/live/andromeda/cs_film-1080.m3u8")
-    add_directory_item("CS Mystery", "play", icon="https://www.jojgroup.sk/wp-content/uploads/CS-mistery.png", is_folder=False, video_url="https://live.cdn.joj.sk/live/andromeda/cs_mystery-1080.m3u8")
-    add_directory_item("Prima Love", "play", icon="https://www.recenzer.cz/wp-content/uploads/2023/10/prima-love-logo.jpg", is_folder=False, video_url="http://88.212.15.19/live/prima_love_avc_25p/playlist.m3u8")
-    add_directory_item("TV LUX", "play", icon="https://213.sk/wp-content/uploads/2020/11/tvlux.jpg", is_folder=False, video_url="https://stream.tvlux.sk/luxtv/luxtv-livestream/playlist.m3u8")
-    add_directory_item("TV Liptov", "play", icon="https://yt3.googleusercontent.com/JJ6maA0dhvLU3z45Jhbgcc1brVZQswuPfYS6Da-Gli4MxXEPlhz5yuLkJlp7VL7mG7eSIxBORA=s900-c-k-c0x00ffffff-no-rj", is_folder=False, video_url="http://95.105.255.137:1935/tvturiec/tvliptov.stream/playlist.m3u8")
-    add_directory_item("TV Nitrička", "play", icon="https://www.satelitnatv.sk/wp-content/uploads/2013/04/nitricka.jpg", is_folder=False, video_url="https://dash4.antik.sk/live/test_nitricka/playlist.m3u8")
-    add_directory_item("TV9", "play", icon="https://www.fotelka.tv/image/cache/catalog/Regionalne/TV9-240x234.jpg", is_folder=False, video_url="https://dash4.antik.sk/live/test_tv9/playlist.m3u8")
-    add_directory_item("TV 8", "play", icon="https://www.digislovakia.sk/wp-content/uploads/2023/04/TV8-logo-2-300x231.png", is_folder=False, video_url="http://109.74.145.11:1935/tv8/ngrp:tv8.stream_all/playlist.m3u8")
-    add_directory_item("Senzi TV", "play", icon="https://static.wikia.nocookie.net/cstv/images/8/85/Senzi.png", is_folder=False, video_url="https://lb.streaming.sk/senzi/stream/playlist.m3u8")
-    add_directory_item("Flow TV", "play", icon="https://www.flowtv.sk/wp-content/uploads/2021/04/logo_flow_tv_web.png", is_folder=False, video_url="https://app.viloud.tv/hls/channel/04e456809c83928443e59f0a2fce8610.m3u8")
+    for name, logo, tid, url in CHANNELS_SK:
+        add_directory_item(name, "play", icon=logo, is_folder=False, video_url=url, tvg_id=tid)
     xbmcplugin.endOfDirectory(HANDLE)
 
 def list_czech_channels():
-    """Zoznam českých staníc."""
-    add_directory_item("Minimax", "play", icon="https://www.minimaxcz.tv/storage/images/cWiGhWyxj8fFnyWQZxEX.png", is_folder=False, video_url="http://88.212.15.19/live/test_minimax/playlist.m3u8")
-    add_directory_item("Óčko", "play", icon="https://parasite.cz/wp-content/uploads/2013/02/ocko1.jpg", is_folder=False, video_url="https://ocko-live-dash.ssl.cdn.cra.cz/cra_live2/ocko.stream.1.smil/playlist.m3u8")
-    add_directory_item("ČT 24", "play", icon="https://pecka.tv/wp-content/uploads/2025/12/studio-ct24-400x600.jpg", is_folder=False, video_url="https://dash2.antik.sk/live/ct24_avc_25p/playlist.m3u8")
-    
-    # NOVÁ STANICA: ČT Sport
-    add_directory_item("ČT Sport", "play", icon="https://www.itelka.sk/wp-content/uploads/2023/04/ct-sport.png", is_folder=False, video_url="http://88.212.15.19/live/test_ctsport_25p/playlist.m3u8")
-    
+    for name, logo, tid, url in CHANNELS_CZ:
+        add_directory_item(name, "play", icon=logo, is_folder=False, video_url=url, tvg_id=tid)
     xbmcplugin.endOfDirectory(HANDLE)
 
 def play_video(stream_url, title):
-    """Spustí video."""
-    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"
+    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
     referer = "https://www.joj.sk/"
     final_url = f"{stream_url}|User-Agent={urllib.parse.quote(user_agent)}&Referer={urllib.parse.quote(referer)}"
     list_item = xbmcgui.ListItem(path=final_url)
@@ -95,10 +126,12 @@ if __name__ == '__main__':
         list_slovak_channels()
     elif action == 'list_cz':
         list_czech_channels()
+    elif action == 'set_pvr_playlist':
+        setup_pvr_playlist()
+    elif action == 'set_pvr_epg':
+        setup_pvr_epg()
     elif action == 'play':
         play_video(params.get('url'), params.get('title'))
-    elif action in ['set_pvr_playlist', 'set_pvr_epg']:
-        xbmcgui.Dialog().notification('PVR', 'Funkcia sa pripravuje...', xbmcgui.NOTIFICATION_INFO, 5000)
     else:
         show_main_menu()
 
